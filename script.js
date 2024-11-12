40b8f7bdae0eca8053b2eb7198db66d90ec38c94
FREEMAN-LT\stepa <stepansidorov3234@gmail.com>
2024-11-12 21:44:10 +0300

    Добавил полную логику для расчета значений, их проверки и отрисовки, а так же функционал возврата к форме и её очистке. Проект завершен.

--- script.js
+++ script.js
@@ -1,19 +1,10 @@
 let formEl = document.querySelector('.form')
+let resultEl = document.querySelector('.result')
+let backBtnEl = document.querySelector('.btn-back')
+let btnClear = document.querySelector('.btn-clear')
+let inputEls = document.querySelectorAll('input')
+let selectEl = document.querySelector('select')
 
-function calculateCalories(formDataObj){
-    let {gender, age, height, weight, activity, formula} = formDataObj
-
-    if(formula === "Mifflin-St Jeor Equation"){
-        return Math.round(
-            Number(((10 * weight) + ((6.25 * height) - (5 * age) + (gender === 'Male' ? + 5 : -161)) * activity) * (1 + 20 / 100))
-        )
-    } else {
-        return Math.round(
-            Number(((gender === 'Male' ? ((13.397 * weight) + (4.799 * height) - (5.677 * age) + 88.362 * activity) : (9.247  * weight) + (3.098 * height) - (4.330 * age) + 447.593) * activity))
-        )
-    }
-    
-}
 
 formEl.addEventListener('submit', (event) => {
     event.preventDefault()
@@ -21,8 +12,74 @@ formEl.addEventListener('submit', (event) => {
     let formData = new FormData(formEl)
     let formDataObj = Object.fromEntries(formData)
 
-    console.log(formDataObj);
+    calculateCalories(formDataObj);
 
-    console.log(calculateCalories(formDataObj));
+    event.target.reset()
 })
 
+
+function calculateCalories(formDataObj){
+    let {gender, age, height, weight, activity, formula} = formDataObj
+
+    let baseValue = 
+        formula === "Mifflin-St Jeor Equation" 
+        ? 
+        Number(((10 * weight) + ((6.25 * height) - (5 * age) + (gender === 'Male' ? + 5 : -161)) * activity) * 1.20)
+        : 
+        Number(((gender === 'Male' ? ((13.397 * weight) + (4.799 * height) - (5.677 * age) + 88.362 * activity) : (9.247  * weight) + (3.098 * height) - (4.330 * age) + 447.593) * activity))
+    
+
+     return renderResult(baseValue) 
+}
+
+
+function renderResult(baseValue){
+    if(baseValue > 0){
+        resultEl.innerHTML = `
+        <table>
+            <tr>
+                <th>Gain Weght</th>
+                <th>Maintain Weght</th>
+                <th>Loose Weght</th>
+            </tr>
+            <tr>
+                <td>${Math.round(baseValue * 1.20)}</td>
+                <td>${Math.round(baseValue)}</td>
+                <td>${Math.round(baseValue / 1.20)}</td>
+            </tr>
+        </table>
+        <p>The results show a number of daily calorie estimates that can be used as a guideline for how many calories to consume each day to maintain, lose, or gain weight.</p>
+    `
+
+        formEl.style.display = 'none'
+
+        resultEl.classList.add('calculated')
+        resultEl.style.display = 'flex'
+
+        backBtnEl.style.display = 'block'
+    } else {
+        alert('Impossible result. Use valid properties.')
+    }
+}
+
+
+backBtnEl.addEventListener('click', event => {
+    if(resultEl.classList.contains('calculated')){
+        formEl.style.display = 'flex'
+        resultEl.style.display = 'none'
+        backBtnEl.style.display = 'none'
+    }
+})
+
+
+function clearForm(){
+    inputEls.forEach(el => {
+        el.value = ''
+        el.checked = false
+    })
+}
+
+
+btnClear.addEventListener('click', event => {
+    clearForm()
+})
